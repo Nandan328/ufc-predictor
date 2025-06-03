625f56c7170f622912514513c469cac8fd3f21da
Nandan K <nandanyadav328@gmail.com>
2025-06-03 10:29:38 +0530

    added database to store fighter details

--- app/api/get-fighter-details/[fighter]/route.ts
+++ app/api/get-fighter-details/[fighter]/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import findFighterDetails from ".";
+import { prisma } from "@/prisma";
 
 export async function GET(
   request: NextRequest,
@@ -11,5 +12,29 @@ export async function GET(
   if (!fighter_details) {
     return NextResponse.json({ error: "Fighter not found" }, { status: 404 });
   }
+
+  if(!await prisma.fighterData.findFirst({
+    where: {
+      name: fighter_details.name,
+    },
+  })) {
+    const res = await prisma.fighterData.create({
+      data: {
+        name: fighter_details.name,
+        nickname: fighter_details.nickname,
+        tags: fighter_details.tags,
+        division: fighter_details.division,
+        win_lose: fighter_details.win_lose,
+        stats: fighter_details.stats.map((stat) => ({
+          stat: stat.stat,
+          num: stat.num,
+        })),
+        img: fighter_details.img,
+      },
+    });
+
+    console.log("Fighter data created:", res);
+  }
+
   return NextResponse.json(fighter_details);
 }
