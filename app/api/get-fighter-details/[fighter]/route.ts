a6c996fd326a2b733ea2a4816d87fed668a71ffd
Nandan K <nandanyadav328@gmail.com>
2025-06-05 01:10:37 +0530

    Update route.ts

--- app/api/get-fighter-details/[fighter]/route.ts
+++ app/api/get-fighter-details/[fighter]/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import findFighterDetails from ".";
-import { prisma } from "@/prisma";
+// import { prisma } from "@/prisma";
 
 export async function GET(
   request: NextRequest,
@@ -13,32 +13,32 @@ export async function GET(
     return NextResponse.json({ error: "Fighter not found" }, { status: 404 });
   }
 
-  const existingFighter = await prisma.fighterData.findUnique({
-    where: {
-      name: fighter_details.name,
-    },
-  });
+  // const existingFighter = await prisma.fighterData.findUnique({
+  //   where: {
+  //     name: fighter_details.name,
+  //   },
+  // });
 
-  if (!existingFighter) {
-    await prisma.fighterData.create({
-      data: {
-        name: fighter_details.name,
-        nickname: fighter_details.nickname,
-        tags: fighter_details.tags,
-        division: fighter_details.division,
-        win_lose: fighter_details.win_lose,
-        stats: Array.isArray(fighter_details.stats)
-          ? {
-              set: fighter_details.stats.map((stat) => ({
-                stat: stat.stat,
-                num: stat.num,
-              })),
-            }
-          : { set: [] },
-        img: fighter_details.img,
-      },
-    });
-  }
+  // if (!existingFighter) {
+  //   await prisma.fighterData.create({
+  //     data: {
+  //       name: fighter_details.name,
+  //       nickname: fighter_details.nickname,
+  //       tags: fighter_details.tags,
+  //       division: fighter_details.division,
+  //       win_lose: fighter_details.win_lose,
+  //       stats: Array.isArray(fighter_details.stats)
+  //         ? {
+  //             set: fighter_details.stats.map((stat) => ({
+  //               stat: stat.stat,
+  //               num: stat.num,
+  //             })),
+  //           }
+  //         : { set: [] },
+  //       img: fighter_details.img,
+  //     },
+  //   });
+  // }
 
   return NextResponse.json(fighter_details);
 }
